da3bc11ace4f4f236c9127609ea0f753e01147be
Ros Haitovich <matrostik@gmail.com>
2018-09-08 23:40:06 +0300

    login and signup validation

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
+import validator from 'express-validator';
 
 export default app => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(validator());
 };
--- src/controllers/user/controller.js
+++ src/controllers/user/controller.js
@@ -1,12 +1,40 @@
 import UserModel from './model';
 
 const loginPage = async (req, res) => {
-    console.log(req.method);
 
     if (req.method === 'GET') {
-        res.render('pages/user/login');
+        var errors = [];
+        var user = {
+            email: '',
+            password: ''
+        };
+        res.render('pages/user/login', {
+            user,
+            errors
+        });
     } else { // post
+        const body = req.body;
+        console.log(body);
+        // validation here
+        req.checkBody("email", "Enter a valid email address.").isEmail();
+        req.checkBody("password", "Password is required").notEmpty();
 
+        var errors = req.validationErrors();
+        console.log(errors);
+        if (errors) {
+            var user = {
+                email: body.email,
+                password: body.password
+            };
+            res.render('pages/user/login', {
+                user,
+                errors
+            });
+        } else {  // normal processing here
+            console.log('OK');
+            res.render('pages/home');
+
+        }
     }
 }
 
@@ -19,31 +47,77 @@ const signupPage = async (req, res) => {
 
     if (req.method === 'GET') {
         var errors = [];
-        var user = { name: "", lastname: "", email: "", password: "" };
-        res.render('pages/user/signup', { user, errors });
+        var user = {
+            name: '',
+            lastname: '',
+            email: '',
+            password: ''
+        };
+        res.render('pages/user/signup', {
+            user,
+            errors
+        });
     } else { // post
-        // do some validation here
         const body = req.body;
         console.log(body);
-        const userExists = await UserModel.getUser(body.email);
-        //console.log(userExists);
-        if (userExists !== undefined) {
-            // email already registered
-            // error
-            var user = { name: body.name, lastname: body.lastname, email: body.email, password: body.password };
-            var errors = [];
+
+        // validation here
+        req.checkBody("name", "First Name is required").notEmpty();
+        //req.checkBody("name", "First Name must be at least 2 characters").isLength({ min: 2 });
+        req.checkBody("lastname", "Last Name is required").notEmpty();
+        //req.checkBody("lastname", "First Name must be at least 2 characters").isLength({ min: 2 });
+        req.checkBody("email", "Enter a valid email address.").isEmail();
+        req.checkBody("password", "Password is required").notEmpty();
+        req.checkBody("password", "Password must be at least 6 characters").isLength({ min: 6 });
+
+        var errors = req.validationErrors();
+        console.log(errors);
+        if (errors) {
+            //res.send(errors);
+            var user = {
+                name: body.name,
+                lastname: body.lastname,
+                email: body.email,
+                password: body.password
+            };
             console.log(user);
-            res.render('pages/user/signup', { user, errors });
-        } else { // create new user
-            const user = await UserModel.createUser(body);
-            console.log(user);
-            res.render('pages/user/login');
+            res.render('pages/user/signup', {
+                user,
+                errors
+            });
+            console.log('Error');
+            return;
+        } else {  // normal processing here
+            console.log('OK');
+
+            const userExists = await UserModel.getUser(body.email);
+            //console.log(userExists);
+            if (userExists !== undefined) {
+                // email already registered
+                // error
+                var user = {
+                    name: body.name,
+                    lastname: body.lastname,
+                    email: body.email,
+                    password: body.password
+                };
+                var errors = [];
+                console.log(user);
+                res.render('pages/user/signup', {
+                    user,
+                    errors
+                });
+            } else { // create new user
+                const user = await UserModel.createUser(body);
+                console.log(user);
+                res.render('pages/user/login');
+            }
         }
     }
 }
 
 const profilePage = (req, res) => {
-    console.log(req);
+    //console.log(req);
     res.render('pages/user/profile');
 }
 
